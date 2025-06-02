913e71305fe52622240d509f415d7c16653abaab
NightFox64 <91380334+NightFox64@users.noreply.github.com>
2025-06-02 15:53:43 +0300

    Add webhook

--- main.go
+++ main.go
@@ -2,16 +2,20 @@ package main
 
 import (
 	"log"
+	"net/http"
+	"os"
 
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 )
 
 var (
+	bot          *tgbotapi.BotAPI
 	chatCounters = make(map[int64]int)
 )
 
 func main() {
-	bot, err := tgbotapi.NewBotAPI("7843550853:AAE1Ih5G1WuEnKDPSXRj3DaOLB6y8-mhBF8")
+	var err error
+	bot, err = tgbotapi.NewBotAPI(os.Getenv("7843550853:AAE1Ih5G1WuEnKDPSXRj3DaOLB6y8-mhBF8"))
 	if err != nil {
 		log.Panic(err)
 	}
@@ -19,29 +23,56 @@ func main() {
 	bot.Debug = true
 	log.Printf("Бот запущен: %s", bot.Self.UserName)
 
-	u := tgbotapi.NewUpdate(0)
-	u.Timeout = 60
+	// Устанавливаем вебхук
+	webhookURL := "https://dashboard.render.com/web/srv-d0upb4k9c44c73bl5p1g/webhook"
+	wh, err := tgbotapi.NewWebhook(webhookURL)
+	if err != nil {
+		log.Fatalf("Ошибка создания webhook: %v", err)
+	}
 
-	updates := bot.GetUpdatesChan(u)
+	_, err = bot.Request(wh)
+	if err != nil {
+		log.Fatalf("Ошибка установки webhook: %v", err)
+	}
 
+	// Получаем канал обновлений
+	updates := bot.ListenForWebhook("/webhook")
+
+	// Запускаем сервер
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+	go func() {
+		log.Fatal(http.ListenAndServe(":"+port, nil))
+	}()
+	log.Printf("Сервер запущен на порту %s", port)
+
+	// Обрабатываем обновления
 	for update := range updates {
-		if update.Message == nil {
-			log.Printf("FileID: %s", update.Message.Animation.FileID)
-			continue
-		}
-
-		chatID := update.Message.Chat.ID
-		chatCounters[chatID]++
-
-		if chatCounters[chatID]%10 == 0 {
-			sendGif(bot, chatID)
-		}
+		handleUpdate(update)
 	}
 }
 
-func sendGif(bot *tgbotapi.BotAPI, chatID int64) {
-	msg := tgbotapi.NewAnimation(chatID, tgbotapi.FileID("CgACAgIAAxkBAAMDaD2NxM7H1-jBWRjBHYxlIxNOWIkAAr5zAAKrXvBJaM6laThye-g2BA"))
+func handleUpdate(update tgbotapi.Update) {
+	if update.Message == nil {
+		// Проверяем наличие анимации в сообщении
+		if update.Message != nil && update.Message.Animation != nil {
+			log.Printf("FileID: %s", update.Message.Animation.FileID)
+		}
+		return
+	}
 
+	chatID := update.Message.Chat.ID
+	chatCounters[chatID]++
+
+	if chatCounters[chatID]%10 == 0 {
+		sendGif(chatID)
+	}
+}
+
+func sendGif(chatID int64) {
+	msg := tgbotapi.NewAnimation(chatID, tgbotapi.FileID("CgACAgIAAxkBAAMDaD2NxM7H1-jBWRjBHYxlIxNOWIkAAr5zAAKrXvBJaM6laThye-g2BA"))
 	_, err := bot.Send(msg)
 	if err != nil {
 		log.Printf("Ошибка отправки гифки: %v", err)
